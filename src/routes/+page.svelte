432a6eed14d3e0701ff33d8bef48f5690b05d1bb
Daria Mishalkina <dashaio251@gmail.com>
2023-08-24 19:50:24 +0200

    modal mobile

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,22 @@
 <script>
-    import RulesIcon from '../components/svgs/RulesIcon.svelte';
+    import CloseIcon from "../components/svgs/CloseIcon.svelte";
+    import RulesIcon from "../components/svgs/RulesIcon.svelte";
+    import { score } from "../stores/score.ts";
+
     let areRulesOpened = false;
-    let score = 0;
-    const roles = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
+    const roles = ["rock", "paper", "scissors", "lizard", "spock"];
     const playBible = {
-        'rock': ['lizard', 'scissors'],
-        'scissors': ['paper', 'lizard'],
-        'paper': ['rock', 'spock'],
-        'lizard': ['paper', 'spock'],
-        'spock': ['rock', 'scissors']
+        "rock": ["lizard", "scissors"],
+        "scissors": ["paper", "lizard"],
+        "paper": ["rock", "spock"],
+        "lizard": ["paper", "spock"],
+        "spock": ["rock", "scissors"]
     }
-    // let chatGPTResponse = '';
-    // import axios from 'axios';
-    let playerMove = '';
-    let computerMove = '';
-    let resultMessage = '';
+    // let chatGPTResponse = "";
+    // import axios from "axios";
+    let playerMove = "";
+    let computerMove = "";
+    let resultMessage = "";
     // let conversationHistory = [];
 
     // export async function getChatGPTResponse(playerMove, conversationHistory) {
@@ -22,16 +24,16 @@
     //         const response = await axios.post(
     //             VITE_OPENAI_ENDPOINT,
     //             {
-    //                 model: 'gpt-3.5-turbo-0301',
+    //                 model: "gpt-3.5-turbo-0301",
     //                 // prompt: `Player chose ${playerMove}. ChatGPT:`,
     //                 max_tokens: 50,
     //                 temperature: 0.7,
-    //                 messages: [{role: 'user', content: 'Say this is a test!'}]
+    //                 messages: [{role: "user", content: "Say this is a test!"}]
     //             },
     //             {
     //                 headers: {
-    //                     'Content-Type': 'application/json',
-    //                     'Authorization': `Bearer ${VITE_OPENAI_API_KEY}`,
+    //                     "Content-Type": "application/json",
+    //                     "Authorization": `Bearer ${VITE_OPENAI_API_KEY}`,
     //                 },
     //             }
     //         );
@@ -39,7 +41,7 @@
     //
     //         return response.data.choices[0].text;
     //     } catch (error) {
-    //         console.error('Error calling OpenAI API:', error);
+    //         console.error("Error calling OpenAI API:", error);
     //         throw error;
     //     }
     // }
@@ -50,29 +52,48 @@
         }
         else if (playBible[computerMove].indexOf(playerMove) > -1){
             resultMessage = "You lose";
-        } else  resultMessage = "You win";
-        // conversationHistory.push({ role: 'player', content: playerMove });
+        } else  {
+            resultMessage = "You win";
+            const actualScore = Number($score) + 1;
+            score.set(String(actualScore))
+        }
+        // conversationHistory.push({ role: "player", content: playerMove });
         //
         // const chatGptResponse = await getChatGPTResponse(playerMove, conversationHistory);
-        // conversationHistory.push({ role: 'chatGPT', content: chatGptResponse });
+        // conversationHistory.push({ role: "chatGPT", content: chatGptResponse });
         // console.log(conversationHistory);
         // Update UI or game logic with the chatGptResponse
+    }
+    const handlePlayAgainButton = () => {
+        computerMove = "";
+        playerMove = "";
+        resultMessage = "";
     }
 
 </script>
 
 <div>
-    <header class=''>
+    <header class="">
         <h1>Rock Paper Scissors Lizard Spock</h1>
         <div>
             <p>Score</p>
-            <p>{score}</p>
+            <p>{$score}</p>
         </div>
     </header>
-    <main>
+    <main class="main">
         {#if areRulesOpened}
-            <section class=''>
-                <RulesIcon />
+            <section class="modal__wrapper">
+                <div class="modal__container">
+                    <h2>Rules</h2>
+                    <RulesIcon />
+                    <button
+                            class="close-button modal__close-button"
+                            on:click={() => areRulesOpened = false}
+                    >
+                        <span class="visually-hidden-title">Close Rules</span>
+                        <CloseIcon />
+                    </button>
+                </div>
             </section>
         {/if}
         <ul>
@@ -89,27 +110,115 @@
             <p>{computerMove}</p>
         {/if}
         {#if resultMessage.length > 0}
-            <p>{resultMessage}</p>
+            <section>
+                <p>{resultMessage}</p>
+                <button on:click={() => handlePlayAgainButton()}>Play again</button>
+            </section>
+
         {/if}
-        <label>
-            <input
-                    on:change|preventDefault={() => areRulesOpened = !areRulesOpened}
-                    type='checkbox'
-                    checked={areRulesOpened}
-            />
-            rules
-        </label>
     </main>
+    <label class="rules-checkbox">
+        <input
+                class="rules-checkbox__input"
+                on:change|preventDefault={() => areRulesOpened = !areRulesOpened}
+                type="checkbox"
+                checked={areRulesOpened}
+        />
+        rules
+    </label>
 </div>
 
 <style>
+    *, *::before, *::after {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
     :global(body) {
-        font-family: 'Barlow Semi Condensed', sans-serif;
+        font-family: "Barlow Semi Condensed", sans-serif;
         background: radial-gradient(hsl(214, 47%, 23%), hsl(237, 49%, 15%));
         height: 100vh;
         overflow: hidden;
         --text-color: #fff;
+        --border-color: hsl(217, 16%, 45%);
+        --dark-text-color: hsl(229, 25%, 31%);
         color: var(--text-color);
         text-transform: uppercase;
+        margin: 0;
+        padding: 0;
+        font-weight: 600;
     }
+    :global(button) {
+        cursor: pointer;
+        padding: 0;
+        margin: 0;
+        background: none;
+        border: none;
+    }
+    :global(button):focus {
+        outline: none;
+    }
+    .main {
+        position: relative;
+        display: flex;
+    }
+    
+    .rules-checkbox {
+        position: absolute;
+        bottom: 55px;
+        right: 50%;
+        transform: translate(50%, 0);
+        font-size: 16px;
+        padding: 15px 38px 12px 38px;
+        border: 2px solid var(--border-color);
+        border-radius: 10px;
+        cursor: pointer;
+        letter-spacing: 0.25em;
+    }
+    .rules-checkbox__input {
+        height: 0;
+        width: 0;
+        visibility: hidden;
+    }
+    .modal__wrapper {
+        position: fixed;
+        top: 0;
+        left: 0;
+        bottom: 0;
+        right: 0;
+        background: rgba(0, 0, 0, 75%);
+        z-index: 100;
+    }
+    .modal__container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: space-around;
+        height: 100vh;
+        background: #fff;
+        color: var(--dark-text-color);
+        /*position: absolute;*/
+        /*top: 50%;*/
+        /*left: 50%;*/
+        /*transform:  translate(-50%, -50%);*/
+        /*width: fit-content;*/
+        /*height: fit-content;*/
+    }
+    .visually-hidden-title:not(:focus):not(:active) {
+        clip: rect(0 0 0 0);
+        clip-path: inset(100%);
+        height: 1px;
+        overflow: hidden;
+        position: absolute;
+        white-space: nowrap;
+        width: 1px;
+    }
+    .close-button {
+        display: flex;
+    }
+    .close-button :global(svg) {
+        width: 20px;
+        height: 20px;
+    }
+
 </style>
---
+++ src/stores/score.ts
@@ -0,0 +1,18 @@
+import {writable} from 'svelte/store';
+import {browser} from "$app/environment";
+
+let stored = "";
+
+if(browser){
+    stored = localStorage.getItem('score') || '';
+}
+
+
+
+export const score = writable(stored || "0");
+
+score.subscribe((value) => {
+    if(browser){
+        localStorage.setItem('score', value);
+    }
+});
