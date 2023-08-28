1eab518dd686421653f12fdb86e2c9b902d3aae3
Daria Mishalkina <dashaio251@gmail.com>
2023-08-28 17:00:30 +0200

    role chips mobile

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,8 @@
 <script>
     import CloseIcon from "../components/svgs/CloseIcon.svelte";
     import Lizard from "$lib/assets/icon-lizard.svg";
+    import Logo from "$lib/assets/logo-bonus.svg"
     import Paper from "$lib/assets/icon-paper.svg";
-    import Pentagon from "$lib/assets/bg-pentagon.svg";
     import Rock from "$lib/assets/icon-rock.svg";
     import RulesIcon from "../components/svgs/RulesIcon.svelte";
     import Scissors from "$lib/assets/icon-scissors.svg";
@@ -10,6 +10,21 @@
     import Spock from "$lib/assets/icon-spock.svg";
     let areRulesOpened = false;
     const roles = {
+        "scissors": {
+            imageUrl: Scissors,
+            defeat:  ["paper", "lizard"],
+            color: "linear-gradient(hsl(39, 89%, 49%), hsl(40, 84%, 53%))"
+        },
+        "spock": {
+            imageUrl: Spock,
+            defeat: ["rock", "scissors"],
+            color: "linear-gradient(hsl(189, 59%, 53%), hsl(189, 58%, 57%))"
+        },
+        "lizard": {
+            imageUrl: Lizard,
+            defeat: ["paper", "spock"],
+            color: "linear-gradient(hsl(261, 73%, 60%), hsl(261, 72%, 63%))"
+        },
         "rock": {
             imageUrl: Rock,
             defeat: ["lizard", "scissors"],
@@ -19,21 +34,6 @@
             imageUrl: Paper,
             defeat:  ["rock", "spock"],
             color: "linear-gradient(hsl(230, 89%, 62%), hsl(230, 89%, 65%))"
-        },
-        "scissors": {
-            imageUrl: Scissors,
-            defeat:  ["paper", "lizard"],
-            color: "linear-gradient(hsl(39, 89%, 49%), hsl(40, 84%, 53%))"
-        },
-        "lizard": {
-            imageUrl: Lizard,
-            defeat: ["paper", "spock"],
-            color: "linear-gradient(hsl(261, 73%, 60%), hsl(261, 72%, 63%))"
-        },
-        "spock": {
-            imageUrl: Spock,
-            defeat: ["rock", "scissors"],
-            color: "linear-gradient(hsl(189, 59%, 53%), hsl(189, 58%, 57%))"
         }
     };
     // let chatGPTResponse = "";
@@ -70,17 +70,19 @@
     //     }
     // }
     async function handlePlayerMove() {
-        computerMove = Object.keys(roles)[Math.floor(Math.random() * 5)];
-        if (computerMove === playerMove) {
-            resultMessage = "Draw";
-        }
-        else if (roles[computerMove].defeat.indexOf(playerMove) > -1){
-            resultMessage = "You lose";
-        } else  {
-            resultMessage = "You win";
-            const actualScore = Number($score) + 1;
-            score.set(String(actualScore))
-        }
+        setTimeout(async () => {
+            computerMove = Object.keys(roles)[Math.floor(Math.random() * 5)];
+            if (computerMove === playerMove) {
+                resultMessage = "Draw";
+            }
+            else if (roles[computerMove].defeat.indexOf(playerMove) > -1){
+                resultMessage = "You lose";
+            } else  {
+                resultMessage = "You win";
+                const actualScore = Number($score) + 1;
+                score.set(String(actualScore))
+            }
+        }, 5000);
         // conversationHistory.push({ role: "player", content: playerMove });
         //
         // const chatGptResponse = await getChatGPTResponse(playerMove, conversationHistory);
@@ -98,11 +100,7 @@
 
 <div class="content-container">
     <header class="header">
-        <h1 class="content-container__title">
-            {#each Object.keys(roles) as role}
-                {role}<br/>
-            {/each}
-        </h1>
+        <img class="header__logo" src={Logo} alt="Rock Paper Scissors Lizard Spock" />
         <div class="score-container header__score-container">
             <p class="score-container__title">Score</p>
             <p class="score-container__value">{$score}</p>
@@ -125,26 +123,21 @@
             </section>
         {/if}
         <section class="game-board main__game-board">
-            <img
-                    class="game-board__bg"
-                    src={Pentagon}
-                    alt="Pentagon background"
-            />
             {#if playerMove.length === 0}
                 <ul class="roles-list game_board__roles-list">
                     {#each Object.entries(roles) as [role, {imageUrl, color}], i (i)}
                         <li class="role roles-list__item">
                             <button
-                                    class="role__button"
+                                    class="role-chip"
                                     style="background: {color}"
                                     on:click={() => {
                                         playerMove = role;
                                         handlePlayerMove()
                                     }}
                             >
-                                <span class="role__image-wrapper">
+                                <span class="role-chip__image-wrapper">
                                      <img
-                                             class="role__icon"
+                                             class="role-chip__icon"
                                              src={imageUrl}
                                              alt={role}
                                      />
@@ -154,20 +147,60 @@
                     {/each}
                 </ul>
             {:else}
-                <div>
-                    <div>{playerMove}</div>
-                    <div>
-                        {#if computerMove.length > 0}
-                            <p>{computerMove}</p>
-                        {/if}
+                <div class="action-container game-board__action-container">
+                    <ul class="moves action-container__moves">
+                        <li class="moves__item">
+                            <div
+                                    class="role-chip"
+                                    style="background: {roles[playerMove].color}"
+                            >
+                                <div class="role-chip__image-wrapper">
+                                    <img
+                                            class="role-chip__icon"
+                                            src={roles[playerMove].imageUrl}
+                                            alt={playerMove}
+                                    />
+                                </div>
+                            </div>
+                            <p>You picked</p>
+                        </li>
+                        <li class="move moves__item">
+                            <!--{#if computerMove.length > 0}-->
+                            <!--    <div-->
+                            <!--            class="role-chip computer-move"-->
+                            <!--            style="background: {roles[computerMove].color}"-->
+                            <!--    >-->
+                            <!--        <div class="role-chip__image-wrapper">-->
+                            <!--            <img-->
+                            <!--                    class="role-chip__icon"-->
+                            <!--                    src={roles[computerMove].imageUrl}-->
+                            <!--                    alt={computerMove}-->
+                            <!--            />-->
+                            <!--        </div>-->
+                            <!--    </div>-->
+                            <!--{:else}-->
+                            <!--    <div class="move__empty-content">-->
+
+                            <!--    </div>-->
+                            <!--{/if}-->
+                            <div class="move__empty-content">
+
+                            </div>
+                            <p>The house picked</p>
+                        </li>
+                    </ul>
+                    <div
+                            class="action-container__float-content"
+                            class:hidden={resultMessage.length === 0}
+                    >
+                        <p class="action-container__message">{resultMessage}</p>
+                        <button
+                                class="action-container__button"
+                                on:click={() => handlePlayAgainButton()}>Play again</button>
                     </div>
                 </div>
             {/if}
         </section>
-        <div class:hidden={resultMessage.length === 0}>
-            <p>{resultMessage}</p>
-            <button on:click={() => handlePlayAgainButton()}>Play again</button>
-        </div>
     </main>
     <label class="rules-checkbox">
         <input
@@ -292,10 +325,9 @@
         align-items: center;
         padding: 10px 10px 10px 22px;
     }
-    .content-container__title {
-        font-weight: 700;
-        font-size: 16px;
-        line-height: 16px;
+    .header__logo {
+        width: 48px;
+        height: 48px;
     }
     .header__score-container {
         background: #fff;
@@ -320,58 +352,55 @@
     .main {
         display: flex;
         flex-direction: column;
-        justify-content: space-around;
         flex: 1;
     }
 
     .game-board {
-        position: relative;
         display: flex;
         justify-content: center;
-        align-items: center;
-    }
-    .game-board__bg {
-        max-width: 100%;
+        flex: 1;
     }
     .roles-list {
-       position: absolute;
-        top: 0;
-        bottom: 0;
-        right: 0;
-        left: 0;
+        background-image: url("../lib/assets/bg-pentagon.svg");
+        background-repeat: no-repeat;
+        background-size: 209px 215px;
+        background-position: center;
+        height: 310px;
+        width: 310px;
+        position: relative;
+        align-self: center;
     }
     .role {
         position: absolute;
     }
-    .role:nth-child(1){
+    .role:nth-child(1) {
         top: 0;
         left: 50%;
-        transform: translate(-50%, -25%);
+        transform: translate(-50%, 0);
     }
-    .role:nth-child(2){
-        left: 0;
+    .role:nth-child(2) {
         top: 50%;
+        left: 0;
         transform: translate(0, -75%);
     }
-    .role:nth-child(3){
-        right: 0;
+    .role:nth-child(3) {
         top: 50%;
+        right: 0;
         transform: translate(0, -75%);
     }
-    .role:nth-child(4){
+    .role:nth-child(4) {
+        bottom: 0;
         left: 0;
-        bottom: 0;
-        transform: translate(50%, 25%);
+        transform: translate(50%, 0);
     }
-    .role:nth-child(5){
+    .role:nth-child(5) {
+        bottom: 0;
         right: 0;
-        bottom: 0;
-        transform: translate(-50%, 25%);
+        transform: translate(-50%, 0);
     }
 
 
-
-    .role__button {
+    .role-chip {
         padding: 11px;
         border-radius: 100px;
         height: 97px;
@@ -380,8 +409,9 @@
         box-sizing: border-box;
         justify-content: center;
         align-items: center;
+        box-shadow: rgba(50, 50, 93, 0.25) 0 50px 100px -20px, rgba(0, 0, 0, 0.3) 0 30px 60px -30px, rgba(10, 37, 64, 0.35) 0 -2px 6px 0 inset;
     }
-    .role__image-wrapper {
+    .role-chip__image-wrapper {
         background: #fff;
         border-radius: 100px;
         width: 100%;
@@ -391,8 +421,32 @@
         align-items: center;
         box-sizing: border-box;
     }
-    .role__icon {
+    .role-chip__icon {
         max-height: 40px;
         max-width: 42px;
+    }
+    .action-container {
+        display: flex;
+        flex-direction: column;
+        flex: 1;
+        justify-content: space-around;
+    }
+    .moves {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+    .moves__item {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 28px;
+    }
+    .move__empty-content {
+        height: 97px;
+        width: 97px;
+        border-radius: 100px;
+        background: #1A2946;
+        box-shadow: rgba(50, 50, 93, 0.25) 0 30px 60px -12px inset, rgba(0, 0, 0, 0.3) 0 18px 36px -18px inset;
     }
 </style>
